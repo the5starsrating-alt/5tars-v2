0653f25f5b816c0965770a7d1526683ba8e38a5a
the5starsrating-alt <265631358+the5starsrating-alt@users.noreply.github.com>
2026-04-07 23:43:23 +0300

    add env.js

---
+++ env.js
@@ -0,0 +1,4 @@
+window.FIVEENV = {
+  SUPABASE_URL: 'https://bxlvcdfqpkxyrqjdnchi.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4bHZjZGZxcGt4eXJxamRuY2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1Nzk1MTgsImV4cCI6MjA5MTE1NTUxOH0.sG1TRiia7E6DGAUHLNAakjiLRu3lopDfRNcMB-0kV3U'
+};
